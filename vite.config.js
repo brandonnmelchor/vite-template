a561cd344d7ac6a7af020006e70e70fd09f3b337
Brandonn Melchor <brandonnmelchor@gmail.com>
2025-06-12 10:51:55 -0500

    add configs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,46 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import AutoImport from 'unplugin-auto-import/vite'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ command, mode }) => {
+	const environment = loadEnv(mode, process.cwd())
+
+	console.log('current mode:', command)
+	console.log('current environment:', environment)
+
+	return defineConfig({
+		plugins: [
+			react(),
+
+			AutoImport({
+				imports: ['react', 'react-router-dom'],
+				dirs: ['src/store'],
+
+				eslintrc: {
+					enabled: true,
+					filepath: './.eslintrc-auto-import.json'
+				}
+			})
+		],
+
+		base: environment.VITE_PUBLIC_PATH,
+		mode: mode,
+
+		resolve: {
+			alias: {
+				'@': resolve(__dirname, './src')
+			}
+		},
+
+		server: {
+			proxy: {
+				'/api': {
+					target: 'http://localhost:5000', // bm-note: update as needed
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/api/, '')
+				}
+			}
+		}
+	})
+}
